5254c9a05f7ca71cae447841d500f8ccacab4b3f
Michael Matloka <michael@posthog.com>
2024-09-16 17:57:06 +0200

    feat(environments): Environment switcher, behind flag

--- posthog/api/team.py
+++ posthog/api/team.py
@@ -120,6 +120,7 @@ class TeamSerializer(serializers.ModelSerializer, UserPermissionsSerializerMixin
             "id",
             "uuid",
             "organization",
+            "project_id",
             "api_token",
             "app_urls",
             "name",
@@ -171,6 +172,7 @@ class TeamSerializer(serializers.ModelSerializer, UserPermissionsSerializerMixin
             "id",
             "uuid",
             "organization",
+            "project_id",
             "api_token",
             "created_at",
             "updated_at",
@@ -273,6 +275,7 @@ class TeamSerializer(serializers.ModelSerializer, UserPermissionsSerializerMixin
         return super().validate(attrs)
 
     def create(self, validated_data: dict[str, Any], **kwargs) -> Team:
+        validated_data["project_id"] = self.context["project_id"]
         serializers.raise_errors_on_nested_writes("create", self, validated_data)
         request = self.context["request"]
 
